0a0bc662d7add4ded12a584a7719311a1e06e98d
meibispro <148724280+meibispro@users.noreply.github.com>
2023-10-29 00:13:20 -0500

    Create arplaycod.js

---
+++ webig/codofplay/arplaycod.js
@@ -0,0 +1,117 @@
+ var art = new Artplayer({
+            container: '#artplayer-app',
+            url: video_playlist[0].link,
+            poster: video_playlist[0].poster,
+            setting: true, // habilita el panel de configuración
+            flip: true, // habilita el control de voltear el video
+              settings: [
+        {
+            width: 200,
+            html: 'Subtitle',
+            tooltip: 'Bilingual',
+            icon: '<img width="22" heigth="22" src="https://www.artplayer.org/assets/img/subtitle.svg">',
+            selector: [
+                {
+                    html: 'Display',
+                    tooltip: 'Show',
+                    switch: true,
+                    onSwitch: function (item) {
+                        item.tooltip = item.switch ? 'Hide' : 'Show';
+                        art.subtitle.show = !item.switch;
+                        return !item.switch;
+                    },
+                },
+                {
+                    default: true,
+                    html: 'Bilingual',
+                    url: 'https://www.artplayer.org/assets/sample/subtitle.srt',
+                },
+                {
+                    html: 'Chinese',
+                    url: 'https://www.artplayer.org/assets/sample/subtitle.cn.srt',
+                },
+                {
+                    html: 'Japanese',
+                    url: 'https://www.artplayer.org/assets/sample/subtitle.jp.srt',
+                },
+            ],
+            onSelect: function (item) {
+                art.subtitle.switch(item.url, {
+                    name: item.html,
+                });
+                return item.html;
+            },
+        },
+        {
+            html: 'Switcher',
+            icon: '<img width="22" heigth="22" src="https://www.artplayer.org/assets/img/state.svg">',
+            tooltip: 'OFF',
+            switch: false,
+            onSwitch: function (item) {
+                item.tooltip = item.switch ? 'OFF' : 'ON';
+                console.info('You clicked on the custom switch', item.switch);
+                return !item.switch;
+            },
+        },
+        {
+            html: 'Slider',
+            icon: '<img width="22" heigth="22" src="https://www.artplayer.org/assets/img/state.svg">',
+            tooltip: '5x',
+            range: [5, 1, 10, 0.1],
+            onRange: function (item) {
+                return item.range + 'x';
+            },
+        },
+    ],
+              layers: [
+        {
+            html: '<img width="30" src="https://render.fineartamerica.com/images/rendered/default/poster/7/8/break/images/artworkimages/medium/2/south-park-oisam.jpg">',
+            click: function () {
+                window.open('https://aimu.app');
+                console.info('You clicked on the custom layer');
+            },
+            style: {
+                position: 'absolute',
+                top: '20px',
+                right: '20px',
+                opacity: '.4',
+            },
+        },
+    ],
+              icons: {
+        loading: '<img src="https://www.artplayer.org/assets/img/ploading.gif">',
+        state: '<img width="150" heigth="150" src="https://www.artplayer.org/assets/img/state.svg">',
+        indicator: '<img width="16" heigth="16" src="https://www.artplayer.org/assets/img/indicator.svg">',
+    },
+              hotkey: true,
+              //screenshot: true,
+              autoplay: false,
+              volume: 0.9,
+              fullscreen: true,
+                fullscreenWeb: true,
+                  miniProgressBar: true,
+                    playsInline: true,
+            playbackRate: true, // habilita el control de la velocidad de reproducción
+            aspectRatio: true, // habilita el control de la relación de aspecto
+            subtitleOffset: true, // habilita el control del desplazamiento de los subtítulos
+              
+            // puedes agregar más opciones según tus preferencias
+        });
+art.on('ready', () => {
+    art.notice.show = 'hola';
+})
+        var playlist = document.querySelector('#playlist');
+        video_playlist.forEach(function(video){
+            var li = document.createElement('li');
+            li.setAttribute('movieurl', video.link);
+            li.setAttribute('moviesposter', video.poster);
+            li.textContent = video.name;
+            playlist.appendChild(li);
+        });
+
+        var playlistItems = document.querySelectorAll('#playlist li');
+        playlistItems.forEach(function(item){
+            item.addEventListener('click', function(){
+                art.switchUrl(item.getAttribute('movieurl'), item.getAttribute('moviesposter'));
+            });
+        });
