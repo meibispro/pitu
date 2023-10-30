352eb22c3d5d194484f89f0e9ea28519f81f5824
meibispro <148724280+meibispro@users.noreply.github.com>
2023-10-30 10:55:40 -0500

    Update play20scry.js

--- webig/codofplay/play20scry.js
+++ webig/codofplay/play20scry.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function( $ ) {
   var timer = setInterval(function() {
     // Aquí guardamos el tiempo actual en el almacenamiento local cada 5 segundos
     localStorage.setItem('bvideo-' + btoa(video.src), video.currentTime);
-  }, 5000); // 5000 milisegundos (5 segundos)
+  }, 9000); // 5000 milisegundos (5 segundos)
 
   // Agrega un controlador de eventos para detener el temporizador si el video se pausa manualmente
   $(video).on("pause", function(event) {
